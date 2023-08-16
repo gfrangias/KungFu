ecd993046e684b579d70c0c591daeb283e19d72f
Georgios Frangias <georgios.frangias@outlook.com>
2023-08-16 21:25:10 +0300

    Moving on with Linear FDA implementation

--- examples/naive_fda_mnist_gradient_tape.py
+++ examples/naive_fda_mnist_gradient_tape.py
@@ -17,9 +17,13 @@ parser.add_argument('--kf-optimizer',
                     type=str,
                     default='my-sync-sgd',
                     help='available options: sync-sgd, async-sgd, sma, my-sync-sgd')
+parser.add_argument('--fda',
+                    type=str,
+                    default='naive',
+                    help='available options: naive, linear, sketch')
 parser.add_argument('--threshold',
                     type=str,
-                    default='10')
+                    default='0.8')
 args = parser.parse_args()
 
 (mnist_images, mnist_labels), _ = \
@@ -81,7 +85,13 @@ def compute_divergence_2_norm(last_sync_model, local_model):
     return tensor_to_tensor_list(norm_2)
 
 @tf.function
-def should_synchronize_models(divergence):
+def compute_xi(second_last_sync_model, last_sync_model):
+    subtraction_list = tensor_lists_subtraction()
+    xi = [t / tf.norm(tf.concat(subtraction_list, axis=0)) for t in subtraction_list]
+    return xi
+
+@tf.function
+def should_synchronize_models_naive(divergence):
     global threshold
     if tf.math.greater(tf.cast(divergence, tf.float32),tf.cast(threshold, tf.float32)):
         return True
@@ -97,14 +107,12 @@ def reset_counter():
     steps_since_sync.assign(0)
 
 @tf.function
-def training_step(images, labels, first_batch, last_sync_model):
+def training_step_naive(images, labels, first_batch, last_sync_model):
     increment_counter()
     # Perform TensorFlow GradientTape
     with tf.GradientTape() as tape:
         probs = mnist_model(images, training=True)
         loss_value = loss(labels, probs)
-
-    updated_last_sync_model = [0.0] * len(mnist_model.trainable_variables)
 
     # Find the local gradients 
     grads = tape.gradient(loss_value, mnist_model.trainable_variables)
@@ -119,7 +127,7 @@ def training_step(images, labels, first_batch, last_sync_model):
     np = tf.cast(current_cluster_size(), tf.float32)
     averaged_divergence = map_maybe(lambda d: d / np, summed_divergences)
 
-    if should_synchronize_models(averaged_divergence) or first_batch:
+    if should_synchronize_models_naive(averaged_divergence) or first_batch:
         tf.print("Steps since last sync: ", steps_since_sync)
         tf.print("Average divergence: ", averaged_divergence)
         reset_counter()
@@ -140,6 +148,49 @@ def training_step(images, labels, first_batch, last_sync_model):
 
     return loss_value, updated_last_sync_model
 
+@tf.function
+def training_step_linear(images, labels, first_batch, last_sync_model, xi):
+    increment_counter()
+    # Perform TensorFlow GradientTape
+    with tf.GradientTape() as tape:
+        probs = mnist_model(images, training=True)
+        loss_value = loss(labels, probs)
+
+    # Find the local gradients 
+    grads = tape.gradient(loss_value, mnist_model.trainable_variables)
+
+    # Apply the new gradients locally
+    opt.apply_gradients(zip(grads, mnist_model.trainable_variables))
+    updated_last_sync_model = last_sync_model
+
+    local_divergence = compute_divergence_2_norm(last_sync_model, mnist_model.trainable_variables)
+    print(local_divergence)
+    summed_divergences = group_all_reduce(local_divergence)
+    np = tf.cast(current_cluster_size(), tf.float32)
+    averaged_divergence = map_maybe(lambda d: d / np, summed_divergences)
+
+    if should_synchronize_models_naive(averaged_divergence) or first_batch:
+        tf.print("Steps since last sync: ", steps_since_sync)
+        tf.print("Average divergence: ", averaged_divergence)
+        reset_counter()
+        summed_models = group_all_reduce(mnist_model.trainable_variables)
+        # Cast the number of workers to tf.float32
+        np = tf.cast(current_cluster_size(), tf.float32)
+        # Reduce the gradients of the current node based on the average of all nodes
+        averaged_models = map_maybe(lambda g: g / np, summed_models)
+        for i, variable in enumerate(mnist_model.trainable_variables):
+            variable.assign(averaged_models[i])
+        xi = compute_xi(updated_last_sync_model, mnist_model.trainable_variables)   # Compute the unit vector needed for Linear FDA
+        updated_last_sync_model = mnist_model.trainable_variables
+        
+    # KungFu: broadcast is done after the first gradient step to ensure optimizer initialization.
+    if first_batch:
+        from kungfu.tensorflow.initializer import broadcast_variables
+        broadcast_variables(mnist_model.variables)
+        broadcast_variables(opt.variables())
+
+    return loss_value, updated_last_sync_model, xi
+
 last_sync_model = [0.0] * len(mnist_model.trainable_variables)
 
 # KungFu: adjust number of steps based on number of GPUs.
@@ -147,7 +198,16 @@ for batch, (images, labels) in enumerate(
         dataset.take(10000 // current_cluster_size())):
     
     #print("Step #"+str(batch))
-    loss_value, last_sync_model = training_step(images, labels, batch == 0, last_sync_model)
+    if args.fda == 'naive':
+        loss_value, last_sync_model = training_step_naive(images, labels, batch == 0, last_sync_model)
+    elif args.fda == 'linear':
+        loss_value, last_sync_model = training_step_linear(images, labels, batch == 0, last_sync_model)
+    elif args.fda == 'sketch':
+        print("Sketch not implemented yet!")
+        exit()
+    else:
+        print("FDA method \""+args.fda+"\" isn't available!")
+        exit()
 
     if batch % 10 == 0 and current_rank() == 0:
         print('Step #%d\tLoss: %.6f' % (batch, loss_value))
