b3c676a22fb483e8d0120030e8f594fce362e64b
Vasilis Samoladas <vsam@softnet.tuc.gr>
2023-11-28 10:39:38 +0200

    New experiments results

--- slurm_job.sh
+++ slurm_job.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -l
 
-#SBATCH --job-name=kf.cl16
-#SBATCH --output=kf.cl16.out
-#SBATCH --error=kf.cl16.err
-#SBATCH --ntasks=8
-#SBATCH --nodes=8
+#SBATCH --job-name=kf.cl32
+#SBATCH --output=kf.cl32.out
+#SBATCH --error=kf.cl32.err
+#SBATCH --ntasks=16
+#SBATCH --nodes=16
 #SBATCH --ntasks-per-node=1
 #SBATCH --mem=32G
-#SBATCH --time=4-00:00:00
+#SBATCH --time=1-00:00:00
 #SBATCH --partition=gpu
 #SBATCH --account=pa230902
 
@@ -26,8 +26,8 @@ export TF_XLA_FLAGS="--tf_xla_enable_xla_devices"
 ## RUN YOUR PROGRAM ##
 ip_list=""
 
-num_clients="16"
-num_nodes="8"
+num_clients="32"
+num_nodes="16"
 
 nodelist=$(scontrol show hostnames $SLURM_NODELIST)
 
@@ -42,5 +42,5 @@ done
 
 echo "IP List: $ip_list"
 
-srun python3 run_experiments.py --clients 16 --nodes 8 --ips $ip_list --nic "eth0" --json experiments_16.json
+srun python3 run_experiments.py --clients 32 --nodes 16 --ips $ip_list --nic "eth0" --json experiments_32.json
 
